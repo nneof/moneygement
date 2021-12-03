80a05b898489bbcfbc5f6d466f3d67debfa8911f
Neofytou, Nikolas <Nikolas.Neofytou1@T-mobile.com>
2021-12-03 17:44:07 +0200

    added logo to navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import style from '../styling/NavigationBar.module.css';
+import logo from '../logo.svg'
 
 function NavigationBar() {
     return (
-        <header className={style.header}>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/homepage'>Homepage</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/expenses'>Expenses</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/shopping_list'>Shopping list</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/notes'>Take a note</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/settings'>Settings</NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName={style.active} to='/login'>Logout</NavLink>
-                    </li>   
-                </ul>
-            </nav>
-        </header>
+        <>
+            <a href="./homepage">
+                <img className={style['nav-bar-logo']} src={logo} alt={"logo"} />
+            </a>
+            <header className={style.header}>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/homepage'>Homepage</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/expenses'>Expenses</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/shopping_list'>Shopping list</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/notes'>Take a note</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/settings'>Settings</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to='/login'>Logout</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
