d35aa12388efc54187d98f8189e874c4acaf89ee
Neofytou, Nikolas <Nikolas.Neofytou1@T-mobile.com>
2021-12-06 09:21:18 +0200

    m

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavigationBar from '../components/NavigationBar';
+import '../styling/Homepage.css'
 
 function Homepage() {
     return (
         <div>
             <NavigationBar />
-            <h1>
-                HOMEPAGE
-            </h1>
+            <div className='homepage'>
+                <div className='homepage-expense'>View my expenses</div>
+                <div className='homepage-shopping-list'>View my shopping list</div>
+                <div className='homepage-notes'>View my notes</div>
+                <div className='homepage-future-analysis'>View future analysis</div>
+            </div>
         </div>
     )
 }
